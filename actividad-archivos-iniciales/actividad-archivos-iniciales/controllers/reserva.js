394608025512111133270c5f55d756b18af521df
Lars17S <efren.garibaldi@gmail.com>
2022-05-02 23:45:36 -0500

    minor changes

--- actividad-archivos-iniciales/actividad-archivos-iniciales/controllers/reserva.js
+++ actividad-archivos-iniciales/actividad-archivos-iniciales/controllers/reserva.js
@@ -2,7 +2,7 @@ const Reserva = require("../models/reserva");
 
 exports.reserva_list = function (req, res) {
   Reserva.find({}, function (err, reservas) {
-    if (err) console.log(err);
+    if (err) console.log("error:" + err);
     res.render("reservas/index", { reservas: reservas });
   });
 };
